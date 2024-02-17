ce3896b782ed9093930c3c65d3eec5e766e29285
Haruino <harukiinoue5270@gmail.com>
2024-02-17 08:50:33 +0000

    [Add] Users機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/edit'
-  get 'users/show'
   devise_for :users
   root to: "homes#top"
   get 'homes/about', as: 'about'
-  resources :books, only: [:index, :create, :show, :destroy]
+  resources :books, only: [:index, :create, :show, :destroy, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
