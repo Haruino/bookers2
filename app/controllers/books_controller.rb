96b67e49820db88fc6bb3b335dc06bb5adcae777
Haruino <harukiinoue5270@gmail.com>
2024-02-18 03:23:18 +0000

    [Finish] 全機能実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,29 +8,44 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to books_path
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+    redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      @user = current_user
+      render :index
+    end
   end
-  
+
   def show
     @book = Book.find(params[:id])
     @user = current_user
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
     redirect_to books_path
   end
-  
+
   def edit
+    book = Book.find(params[:id])
+    unless book.user == current_user
+      redirect_to books_path
+    end
     @book = Book.find(params[:id])
   end
-  
+
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book.id)
+    else
+      @user = current_user
+      render :edit
+    end
   end
 
   private
